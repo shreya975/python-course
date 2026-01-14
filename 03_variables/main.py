8cf815007668d0f4afe0f3d9a95be6aff69d20be
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-14 19:49:22 +0530

    Variable and Data type in Python

---
+++ 03_variables/main.py
@@ -0,0 +1,10 @@
+ age = 21 #integer
+name = "shreya"  #string
+height = 5.6 #float
+
+
+# Rule of defining variable in python 
+# Variable name must start with letter (a-z, A-Z) or underscore(_)
+# They can contain letter number and underscores
+# variable names are case sensitive ( age and AGE are different variable)
+# Avoid using python keywords (eg. if , for ,while) as variable name
