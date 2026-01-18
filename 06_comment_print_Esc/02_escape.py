c7b612c4cd7fa458c2a0fb5d52d99fa817961b3d
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-18 19:29:47 +0530

    escape_sequence

---
+++ 06_comment_print_Esc/02_escape.py
@@ -0,0 +1,11 @@
+print("hello, how are you/ \n I am good \\newline")
+
+print("hello  \" world")
+
+print("hello \\ world")
+
+print("hello \t world")
+
+print("hello \" world")
+
+print("hello \' world")
