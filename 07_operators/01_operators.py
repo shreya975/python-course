a975f83f6a9f75aef2e5ed7a9031257d5a8407a4
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-21 22:22:39 +0530

    Arithmetic operators

---
+++ 07_operators/01_operators.py
@@ -0,0 +1,10 @@
+a = 30 
+b = 12
+#Aeithmetic operators
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a // b =", a // b)
+print("a % b =", a % b )
+print("a ** b = ", a ** b)
