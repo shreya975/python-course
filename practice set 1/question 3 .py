b8035801ee058989361409d944dbcd1d126b65bc
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-25 23:52:05 +0530

    .

--- practice set 1/question 3 .py
+++ practice set 1/question 3 .py
@@ -3,4 +3,6 @@ age = 21
 height = 5.7
 is_student = True
 
-print(name , age , height , is_student , sep  = " , ")
+# print(name , age , height , is_student , sep  = " , ")
+
+print("Name of the student is : "  , name ,  "age :" ,  age , "height :" , height , "is_student :"  , is_student )
