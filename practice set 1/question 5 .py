0a7400e2130030c4b7113180a34fa44affe145d5
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-26 10:58:53 +0530

    Taking user input

---
+++ practice set 1/question 5 .py
@@ -0,0 +1,4 @@
+food = input("what is your favourite food? \n")
+
+
+print("wow ! I also like the <food>")
---
+++ practice set 1/question 6.py
@@ -0,0 +1,16 @@
+# a = 20
+# b = 30
+
+
+# print("sum =", a+b)
+# print("Difference =",a-b)
+# print("product =", a*b)
+# print("quotient =", a/b)
+
+a =int(input("Enter first number"))
+b = int(input("Enter second number"))
+
+print("sum =", a+b)
+print("Difference=", a-b)
+print("Product=", a*b)
+print("Quotiebt=", a/b)
