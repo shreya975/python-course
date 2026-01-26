4ae0a4b13fdd023778e0127b2388d6a14a449d67
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-26 10:12:45 +0530

    solving typecasting question

--- practice set 1/question 3 .py
+++ practice set 1/question 3 .py
@@ -5,4 +5,4 @@ is_student = True
 
 # print(name , age , height , is_student , sep  = " , ")
 
-print("Name of the student is : "  , name ,  "age :" ,  age , "height :" , height , "is_student :"  , is_student )
+print("Name of the student is :"  , name ,  ". age :" ,  str(age) , ". height :" , str(height) ,  ". is_student :"  , is_student )
