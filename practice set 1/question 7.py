645e98187affe0eb2f09c31613c43a7eca52b961
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-27 20:03:40 +0530

    completed practice set

---
+++ practice set 1/question 7.py
@@ -0,0 +1,2 @@
+print('Harry said "Python is awesome!"\nThis is on a new line .\nThis is a tab->\t<-here.')
+
---
+++ practice set 1/question 8.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number\n "))
+
+print("the square is",a**2)
+print("the cube is",a**3)
