40baf45a578a05e8415f5b8d276520e12585c912
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-29 23:53:42 +0530

    .

--- 08_Conditionals/if_statement.py
+++ 08_Conditionals/if_statement.py
@@ -1,11 +1,11 @@
-age = 32
-if(age > 18):
-    print("You can drive")
-    print("Thank You")
-
-# age = 12
+# age = 32
 # if(age > 18):
 #     print("You can drive")
 #     print("Thank You")
+
+age = 12
+if(age > 18):
+    print("You can drive")
+    print("Thank You")
 
 print("End of program")
