be5ad65827ed57623235103b46cf4db789e729cf
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-01-29 23:51:14 +0530

    match case

---
+++ 08_Conditionals/match case.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number"))
+
+match a :
+    case 122 :
+        print("Value of 122")
+    case 3 :
+        print("Value of 3")
+    case 6 :
+        print("Value of 6")
+    case _ :    
+        print("Better luck next time")
