3420fbeffc102f5594ca504b86e70ea42d7c2b3c
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-02-25 23:07:04 +0530

    indexing in python

---
+++ 10_strings.py/indexing.py
@@ -0,0 +1,15 @@
+name = "shreya"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[-6])
