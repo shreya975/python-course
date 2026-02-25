e749ebe6b15f1b3d001f8b52f40b12a499ae8d27
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-02-25 23:34:54 +0530

    Slicing in python

---
+++ 10_strings.py/slicing.py
@@ -0,0 +1,16 @@
+name = "Harry012345678"
+
+print(name[0:2]) #goes from 0 to 2-1 ie  0 to 1
+
+print(name[2:-1]) # same as name
+
+print(name[0:10:2]) #skip n-1 characters
+
+print(name[0:10:1]) # skip 0 character
+
+print(name[0:10:3]) # skip 3-1 ie 2 character
+
+print(name[:4]) # Replace the first empty number with 0 # name[0 : 4]
+
+
+print(name[1:]) # Replace the second empty number with lenght #name[1:15]
