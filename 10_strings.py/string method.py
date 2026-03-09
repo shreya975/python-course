2373b1cdeca9f4d791361010bf67daf0645ab79a
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-03-09 22:54:44 +0530

    .

--- 10_strings.py/string method.py
+++ 10_strings.py/string method.py
@@ -1,10 +1,10 @@
-s = "shreya" # String are immutable
+# s = "shreya" # String are immutable
 
-# name[0] = "R" # You cannot do this 
+# # name[0] = "R" # You cannot do this 
 
-a = len(s)
-print(a)
+# a = len(s)
+# print(a)
 
-print(s.upper()) 
-print(s.lower())
-print(s.capitalize())
+# print(s.upper()) 
+# print(s.lower())
+# print(s.capitalize())
