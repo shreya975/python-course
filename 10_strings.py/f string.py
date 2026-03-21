401cc7394826610a4f412483627a92b421662e8b
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-03-21 19:42:15 +0530

    .

--- 10_strings.py/f string.py
+++ 10_strings.py/f string.py
@@ -1,1 +1,15 @@
 # string formatting
+
+template = "Dear {}, you are awesome. Take this {}$ bag"
+
+a = "John"
+a1 = 10000
+
+b = "jack"
+b1 = 1000
+
+c = "Marrie"
+c1 = 100
+
+s1 = template.format(a,a1)
+print(s1)
