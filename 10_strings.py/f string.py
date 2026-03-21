7ddab8fdba15ee42338719143c64da65598d29ee
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-03-21 19:47:07 +0530

    .

--- 10_strings.py/f string.py
+++ 10_strings.py/f string.py
@@ -14,8 +14,8 @@ c1 = 100
 s1 = template.format(a,a1)
 print(s1)
 
-s2 = template.format(b,b1)
-print(s2)
+# s2 = template.format(b,b1)
+# print(s2)
 
-s3 = template.format(c,c1)
-print(s3)
+# s3 = template.format(c,c1)
+# print(s3)
