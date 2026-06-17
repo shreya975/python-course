b063094aa56cf96889b45a60a97cb5314cbeab3d
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-06-17 14:00:00 +0530

    .

--- 11_Oops/main.py
+++ 11_Oops/main.py
@@ -8,12 +8,3 @@ class Person:
         print(f"Age: {self._age}")
 
 
-
-# class Teacher(Person):
-#     def __init__(self, name, age, subject):
-#         super().__init__(name, age)
-#         self.subject = subject
-
-#     def display(self):
-#         super().display()
-        print(f"Subject: {self.subject}")
