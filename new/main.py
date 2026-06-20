de0d396e1a6c5ece4cda46fb8769a1a5db59b7da
Shreya Mahajan <shreyamahajan2911@gmail.com>
2026-06-20 14:00:00 +0530

    .

---
+++ new/main.py
@@ -0,0 +1,18 @@
+print("Summer School")
+
+print (100)
+
+print('100')
+
+# This is single line comment 
+
+'''This is
+multi
+line 
+comment'''
+
+print("Hello")
+print("Hello")
+print("Hello")
+
+print("hello \nhello \nhello \nhello")
